9fc2ea66c5505beae23f4ff301f953702bbccf05
Ruslan Tsitser <cicer_ruslan@mail.ru>
2022-05-04 00:18:47 +0300

    for presentation structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
+import 'presentation/application.dart';
 
 void main() {
   runApp(const Application());
-}
-
-class Application extends StatelessWidget {
-  const Application({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp();
-  }
 }
