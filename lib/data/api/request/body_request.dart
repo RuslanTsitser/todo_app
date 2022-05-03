eb6aa8f8ddf504873bb67b5753d38b148af0855d
Ruslan Tsitser <cicer_ruslan@mail.ru>
2022-05-04 00:17:07 +0300

    for requests

---
+++ lib/data/api/request/body_request.dart
@@ -0,0 +1,3 @@
+abstract class BodyRequest {
+  Map<String, dynamic> toMap();
+}
---
+++ lib/data/api/request/get_user_body.dart
@@ -0,0 +1,10 @@
+import 'body_request.dart';
+
+class GetUserBody extends BodyRequest {
+  GetUserBody();
+
+  @override
+  Map<String, dynamic> toMap() {
+    return {};
+  }
+}
