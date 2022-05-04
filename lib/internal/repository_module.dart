112c40460f1adafe740b31408c3b3d525d424c9e
Ruslan Tsitser <cicer_ruslan@mail.ru>
2022-05-04 19:40:27 +0300

    iternal

---
+++ lib/internal/repository_module.dart
@@ -0,0 +1,16 @@
+import 'package:todo_app/data/repository/auth_data_repository.dart';
+import 'package:todo_app/domain/repository/auth_repository.dart';
+import 'package:todo_app/internal/api_module.dart';
+import 'package:todo_app/internal/shared_module.dart';
+
+class RepositoryModule {
+  static AuthRepository? _authRepository;
+
+  static AuthRepository authRepository() {
+    _authRepository ??= AuthDataRepository(
+      ApiModule.apiUtil(),
+      SharedModule.sharedUtil(),
+    );
+    return _authRepository!;
+  }
+}
---
+++ lib/internal/shared_module.dart
@@ -0,0 +1,12 @@
+import 'package:todo_app/data/shared/service/shared_service.dart';
+
+import '../data/shared/shared_util.dart';
+
+class SharedModule {
+  static SharedUtil? _sharedUtil;
+
+  static SharedUtil sharedUtil() {
+    _sharedUtil ??= SharedUtil(SharedServiceImp());
+    return _sharedUtil!;
+  }
+}
